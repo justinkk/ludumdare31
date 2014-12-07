dc4b1ea9eaa1144ddeed6bd2c59a149b56c0af68
justinkk <justinkk@stanford.edu>
2014-12-07 21:34:23 +0100

    adds all phones to array of spriteFlippers

---
+++ Assets/Scripts/PhoneController.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PhoneController : MonoBehaviour {
+	/** 
+	 * Returns a pair of indices for a phone given the player's indices
+	 * at that location. Don't give it bad coordinates or things will be unhappy
+	 */
+	public static int[] playerIndexToPhoneIndex(int playerX, int playerY) {
+		return new int[] {playerX - 1, playerY / 2};
+	}
+
+	/** 
+	 * Returns a pair of indices for the player given the phone at that location's
+	 * indices. Don't give it bad coordinates or things will be unhappy
+	 */
+	public static int[] phoneIndexToPlayerIndex(int phoneX, int phoneY) {
+		return new int[] {phoneX + 1, phoneY * 2};
+	}
+
+	private SpriteFlipper[,] phones;
+
+	/********Testing
+	*	private float nextTime; //Testing only
+	*	private int currX;
+	*	private int currY;
+	*/
+
+	void Start () {
+		//initialize
+		phones = new SpriteFlipper[5,3];
+
+		for (int x = 0; x < 5; x++) { //left to right
+			for (int y = 0; y < 3; y++) {  //bottom to top
+				//Find the right SpriteFlipper
+				int[] chairIndices = phoneIndexToPlayerIndex(x,y);
+				Transform chair = transform.Find("Chair" + chairIndices[0] + "," + chairIndices[1]);
+				print(chair);
+
+				//phones[x,y] = null;
+				phones[x,y] = chair.Find("Phone").gameObject.GetComponent<SpriteFlipper>();
+			}
+		}
+
+		/*
+		*	//Populate the dictionary
+		*	phones = new Dictionary<Tuple<int,int>, bool>();
+		*	for (r = 0; r <= 4; r+=2) 	//Rows 0, 2, and 4
+		*		for (c = 1; c <= 4; c++)   //Evenry column from 1 to 5
+		*			phones[Tuple.Create(r,c)] = false;
+		*/
+
+		/*
+		 *	nextTime = Time.time + .5f;
+		 *	currX = 0;
+		 *	currY = 0;
+		 */
+	}
+
+	// Update is called once per frame
+	void Update () {
+		/* TESTING
+		if (Time.time > nextTime) {
+			phones[currX,currY].setDark(true);
+
+			nextTime = Time.time + .5f;
+
+			if (currX == 4) {
+				currX = 0;
+				currY++;
+			} else {
+				currX++;
+			}
+		}*/
+	}
+
+	/**Creates a cell phone in the given location and waits for the next one
+	 * Must pass in a valid location for a phone
+	 */
+	/*private void createPhone(int phoneX, int phoneY) {
+		cellPhones[phoneX, phoneY] = true;
+		timeNextPhone();
+	}*/
+
+	//Randomly chooses a location for a new cell phone and generates it
+	/*private void generateRandomPhone() {
+		int phoneX = 0, phoneY = 0;
+
+		//Generate the Y
+		float randomChance = Random.value;
+		if (randomChance < 0.2f)
+			phoneY = 0;
+		else if (randomChance < 0.5f)
+			phoneY = 2;
+		else
+			phoneY = 4;
+
+		//Generate the X
+		randomChance = Random.value;
+		if (randomChance < 0.1f)
+			phoneX = 1;
+		else if (randomChance < 0.4f)
+			phoneX = 2;
+		else if (randomChance < 0.6f)
+			phoneX = 3;
+		else if (randomChance < 0.9f)
+			phoneX = 4;
+
+		//Make the phone
+		createPhone(phoneX, phoneY);
+	}*/
+}
